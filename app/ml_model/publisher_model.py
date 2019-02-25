9bc027855399d35cc5e70ddd975e8eab7320309b
Aaraddhya Bhatalkar <aaraddhya@datatron.io>
2019-02-25 12:22:24 -0800

    Adding healthcheck

--- app/ml_model/publisher_model.py
+++ app/ml_model/publisher_model.py
@@ -2,7 +2,6 @@ import json
 import logging
 import requests
 from app.settings import settings
-from app.monitoring import shiva_statsd
 import datatron.common.transfer as dt
 from datatron.common.ml_parser import get_model
 from datatron.common.ml_parser.exceptions import ModelBinaryLoadError
@@ -11,15 +10,15 @@ from datatron.common.discovery import DatatronDiscovery
 
 class PublisherModelPredictor(object):
     def __init__(self):
-        self.statsd_client = shiva_statsd.get_client()
         self.publisher_slug = settings.PUBLISHER_SLUG
         self.workspace_slug = settings.WORKSPACE_SLUG
+        self.deploy_id = settings.DEPLOYMENT_ID
 
         return
 
     def predict(self, input_features):
         #Prediction code here
 
-        return
+        return 0.5
 
 
--- app/routes/api.py
+++ app/routes/api.py
@@ -1,8 +1,8 @@
 from flask_restful import Api
 api = Api()
 
-
+from app.resources.publisher_healthcheck import PublisherHealthcheck  # noqa
 from app.resources.serve_publisher_request import ServePublisherRequest  # noqa
 
 api.add_resource(ServePublisherRequest, '/predict')
-
+api.add_resource(PublisherHealthcheck, '/healthcheck')
