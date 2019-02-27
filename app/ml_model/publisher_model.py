4db19c083c1b77d78c9abde2e081d9f721fc8b86
Pietro Rancati <pietro@datatron.io>
2019-02-26 18:25:19 -0800

    first version keras model

--- app/ml_model/publisher_model.py
+++ app/ml_model/publisher_model.py
@@ -6,7 +6,7 @@ import datatron.common.transfer as dt
 from datatron.common.ml_parser import get_model
 from datatron.common.ml_parser.exceptions import ModelBinaryLoadError
 from datatron.common.discovery import DatatronDiscovery
-
+from keras.models import model_from_json
 
 class PublisherModelPredictor(object):
     def __init__(self):
@@ -17,8 +17,12 @@ class PublisherModelPredictor(object):
         return
 
     def predict(self, input_features):
-        #Prediction code here
+        # Model reconstruction from JSON file
+        with open('model.json', 'r') as f:
+            model = model_from_json(f.read())
 
-        return 0.5
+        # Load weights into the new model
+        model.load_weights('model.h5')
+        return model.predict(input_features)
 
 
