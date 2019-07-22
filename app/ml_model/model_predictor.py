2e00f394fd4e160358c1918f2fb9cc0d3a698883
Rohan Khade <rohan@datatron.io>
2019-07-22 14:47:44 -0700

    xgboost model for diabetes patients

--- app/ml_model/model_predictor.py
+++ app/ml_model/model_predictor.py
@@ -1,7 +1,8 @@
 import json
 import logging
-from keras import backend as K
-from keras.models import model_from_json
+#from keras import backend as K
+#from keras.models import model_from_json
+import pickle
 
 
 class ModelPredictor(object):
@@ -15,17 +16,18 @@ class ModelPredictor(object):
         :param: x : A list or list of list of input vector
         :return: single prediction
         """
+        model = pickle.load(open("xgboost.pkl", "rb"))
 
-        K.clear_session()
+        #K.clear_session()
 
         # Example of predict function using Keras model
 
         # Model reconstruction from JSON file
-        with open('models/model.json', 'r') as f:
-            model = model_from_json(f.read())
+        #with open('models/model.json', 'r') as f:
+        #    model = model_from_json(f.read())
 
         # Load weights into the new model
-        model.load_weights('models/model.h5')
+        #model.load_weights('models/model.h5')
         return model.predict(x).tolist()
 
     def predict_proba(self, x):
