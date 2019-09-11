3b325c1ae33e1c26d8f63cebac9734cd19a8b6df
Rohan Khade <rohan@datatron.io>
2019-09-11 11:54:13 -0700

    birth xgboost model example

--- app/resources/serve_request.py
+++ app/resources/serve_request.py
@@ -16,11 +16,21 @@ class ServePredictRequest(Resource):
         result = dict(prediction_meta={}, prediction={}, status={})
 
         try:
+            result['prediction_meta'] = {
+                'model_type': settings.MODEL_TYPE,
+                'model_learn_type': settings.LEARN_TYPE,
+                'model_version': settings.MODEL_VERSION,
+                'model_name': settings.MODEL_NAME,
+                'model_slug': settings.MODEL_SLUG,
+                'publisher_slug': settings.PUBLISHER_SLUG,
+                'model_version_slug': settings.MODEL_VERSION_SLUG
+            }
+
             x_dict = request_data['data']
             feature_list = predictor.feature_list()
             x = np.array([[x_dict[feature_name] for feature_name in feature_list]])
             y = predictor.predict(x)
-            result['prediction'] = {'outputs': y[0][0]}
+            result['prediction'] = {'outputs': y[0].item()}
 
             logging.info('Successfully fetched the model prediction result')
             status_msg = "PublisherPredictionSuccess"
