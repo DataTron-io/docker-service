a986fd47199f0a737c06d0ab97ef994bf692f83c
Rohan Khade <rohan@datatron.io>
2019-10-11 10:59:03 -0700

    added asr model

--- app/ml_model/model_predictor.py
+++ app/ml_model/model_predictor.py
@@ -1,7 +1,37 @@
+from __future__ import absolute_import, division, print_function
 import json
 import logging
 import pickle
 import pandas as pd
+import numpy as np
+import shlex
+import subprocess
+import wave
+from deepspeech import Model
+from timeit import default_timer as timer
+
+try:
+    from shhlex import quote
+except ImportError:
+    from pipes import quote
+
+
+BEAM_WIDTH = 500
+
+LM_ALPHA = 0.75
+
+LM_BETA = 1.85
+
+N_FEATURES = 26
+
+N_CONTEXT = 9
+
+
+
+
+
+def metadata_to_string(metadata):
+    return ''.join(item.character for item in metadata.items)
 
 
 class ModelPredictor(object):
@@ -16,9 +46,10 @@ class ModelPredictor(object):
         :return: single prediction
         """
 
-        model = pickle.load(open("models/xgboost_birth_model.pkl", "rb"))
-        x= pd.DataFrame(x, columns = self.feature_list())
-        return model.predict(x)
+        ds = Model('/Users/rohankhade/docker-service/app/ml_model/deepspeech-0.5.1-models/output_graph.pbmm', N_FEATURES, N_CONTEXT, '/Users/rohankhade/docker-service/app/ml_model/deepspeech-0.5.1-models/alphabet.txt', BEAM_WIDTH)
+        ds.enableDecoderWithLM('/Users/rohankhade/docker-service/app/ml_model/deepspeech-0.5.1-models/alphabet.txt', '/Users/rohankhade/docker-service/app/ml_model/deepspeech-0.5.1-models/lm.binary', '/Users/rohankhade/docker-service/app/ml_model/deepspeech-0.5.1-models/trie', LM_ALPHA, LM_BETA)
+        audio = x[0][0].astype(np.int16)
+        return ds.stt(audio, 16000)
 
     def predict_proba(self, x):
         """
@@ -37,5 +68,5 @@ class ModelPredictor(object):
         :param: None
         :return: A list of features
         """
-        return ['Black','Married','Boy','MomAge','MomSmoke','CigsPerDay','MomWtGain','Visit','MomEdLevel']
+        return ['V1']
 
