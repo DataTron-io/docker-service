42f8815bb5e22228c9e0f9e0efffb7f0c41bc76e
Dhruv Kaushal <dhruv@datatron.io>
2020-10-19 21:05:48 -0700

    removed some nesting

--- app/resources/helper.py
+++ app/resources/helper.py
@@ -11,12 +11,12 @@ def fix_structure(input):
 
     output.append(input["input"]["Black"])
     output.append(input["input"]["status"]["Married"])
-    output.append(input["input"]["status"]["info"]["Boy"])
-    output.append(input["input"]["status"]["info"]["MomAge"])
-    output.append(input["input"]["status"]["info"]["MomSmoke"])
+    output.append(input["input"]["status"]["Boy"])
+    output.append(input["input"]["status"]["MomAge"])
+    output.append(input["input"]["status"]["MomSmoke"])
     output.append(input["input"]["status"]["CigsPerDay"])
     output.append(input["input"]["status"]["MomWtGain"])
     output.append(input["Visit"])
-    output.append(input["NestOfNests"]["Nest2"]["Nest3"]["Nest4"]["MomEdLevel"])
+    output.append(input["Test"]["MomEdLevel"])
 
     return output
