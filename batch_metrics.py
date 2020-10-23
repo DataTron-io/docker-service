a17a54538ba114b2803b53fe96bee1a43824b482
Sankalp Patro <sankalp_patro@hotmail.com>
2020-10-22 18:56:30 -0700

    Make file transfers from hdfs

--- batch_metrics.py
+++ batch_metrics.py
@@ -5,8 +5,10 @@ import json
 import logging
 import argparse
 import pandas as pd
+from app.utils import hdfs_transfer as ht
+from app.utils.file_transfer import generate_credentials_for_internal_storage, generate_credentials
 from app.settings import settings
-from app.governor.datatron-metrics.metrics_factory import MetricsManager
+from app.governor.datatron_metrics.metrics_factory import MetricsManager
 
 logging.basicConfig(format=settings.DEFAULT_LOG_FORMAT, level=logging.INFO)
 
@@ -30,7 +32,7 @@ class BatchMetricsJob:
         self.metric_args = settings.METRIC_ARGS
         self.delimiter = chr(int(settings.DELIMITER, 16))
         self.prediction_filepath = settings.REMOTE_INPUT_FILEPATH
-        self.feedback_files = settings.FEEDBACK_FILEPATH_LIST
+        self.feedback_filepaths = settings.FEEDBACK_FILEPATH_LIST
         self.chunk_size = min(self._calculate_byte_row(self.prediction_filepath), settings.CHUNK_SIZE)
         self.prediction_filename = self.prediction_file.rpartition('/')[2]
         metrics_intermediate_dir = os.path.join(settings.METRICS_DIR, self.job_id)
@@ -44,6 +46,47 @@ class BatchMetricsJob:
         avg_mem_usage = (partial_pd.memory_usage(index=False, deep=True) / partial_pd.shape[0]).sum()
         avg_mem_usage *= 8
         return avg_mem_usage
+    
+    @staticmethod
+    def calculate_duration(start_time):
+        return time.time() - start_time
+
+    def _create_local_path(self, remote_path, local_prefix):
+        logging.info(
+            'Creating local filepath for the remote file: {} with prefix: {}'.format(remote_path, local_prefix))
+        remote_filename = remote_path.rpartition('/')[2]
+
+        local_dir = os.path.join(settings.DATATRON_ROOT_LOCATION, self.workspace_slug, local_prefix)
+
+        if not os.path.exists(local_dir):
+            os.makedirs(local_dir, exist_ok=True)
+
+        local_filepath = os.path.join(local_dir, remote_filename)
+
+        logging.info('Successfully created local filepath as: {}'.format(local_filepath))
+
+        return local_filepath
+
+    def fetch_remote_file(self, remote_path, local_prefix):
+        logging.info(
+            'Getting the remote file for batch job from : {}, locally at: {}'.format(remote_path, local_prefix))
+        local_filepath = self._create_local_path(remote_path, local_prefix)
+        if not settings.INPUT_CONNECTOR:
+            credentials = generate_credentials_for_internal_storage()
+        else:
+            credentials = generate_credentials(settings.INPUT_CONNECTOR)
+            logging.info("credentials to use: {}".format(credentials))
+        ht.copy_file(remote_path, local_filepath, 'download', credentials)
+        return local_filepath
+
+    def delete_local_file(self, local_filepath):
+        logging.info(
+            'Deleting the following local file after finishing metric calculation: {}'.format(local_filepath))
+        if os.path.exists(local_filepath):
+            os.remove(local_filepath)
+            logging.info("Successfully deleted file: {}".format(local_filepath))
+        else:
+            logging.info("The file at {} doesn't exist. Nothing was removed.".format(local_filepath))
 
     def find_optimal_chunksize(self, filepath):
         filename = filepath.rpartition('/')[2]
@@ -69,14 +112,23 @@ class BatchMetricsJob:
     def process_batch(self):
         chunksize = 1e6 # Make this a variable in the settings or determine dynamically
         try:
-            for prediction_chunk in pd.read_csv(self.prediction_file, chunksize=chunksize):
-                for feedback_file in self.feedback_files:
-                    for feedback_chunk in pd.read_csv(feedback_file, chunksize=chunksize):
+            local_prediction_filepath = self.fetch_remote_file(remote_path=self.remote_input_filepath, local_prefix='input')
+            logging.info("Successfully received prediction file from {}".format(local_prediction_filepath))
+            for prediction_chunk in pd.read_csv(local_prediction_filepath, 
+                                                chunksize=chunksize, 
+                                                delimiter=self.delimiter):
+                for feedback_filepath in self.feedback_filepaths:
+                    local_feedback_filepath = self.fetch_remote_file(remote_path=feedback_filepath)
+                    logging.info("Successfully received feedback file from {}".format(local_prediction_filepath))
+                    for feedback_chunk in pd.read_csv(local_feedback_filepath, 
+                                                    chunksize=chunksize,
+                                                    delimiter=self.delimiter):
                         try:
                             joined_df = pd.merge(prediction_chunk, feedback_chunk, left_on = "datatron_request_id", right_on = "feedback_id", how="inner")
                             self.metrics_manager.update_batch(joined_df["actual_value"].to_numpy(), joined_df["prediction"].to_numpy()) # VERIFY COLUMN NAMES
                         except KeyError as e:
-                            logging.info(f"Could not join datatron_id column in either the prediction file: {predictions_file} or feedback file: {feedback_file} due to error: {str(e)}.")
+                            logging.info(f"Could not join datatron_id column in either the prediction file: {local_prediction_filepath} or feedback file: {local_feedback_filepath} due to error: {str(e)}.")
+                    self.delete_local_file(local_feedback_filepath)
             metrics_file = os.path.join(self.metrics_dir, self.job_id, self.prediction_filepath)
             metric_values = self.metrics_manager.fetch_metric_values()
             self.save_metrics(metrics_file, metric_values, self.job_id)
