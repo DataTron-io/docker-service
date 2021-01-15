d64a8c14006ac85aa2e7eb09ef9db9c533eb2372
Tyler Gillson <tyler.gillson@gmail.com>
2021-01-15 10:56:34 -0800

    Add /metrics endpoint for prometheus (#20)
    * Ignore PyCharm meta files
    
    * Add /metrics endpoint for prometheus

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import logging
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 from app.settings import settings  # noqa
 
 logging.basicConfig(format=settings.DEFAULT_LOG_FORMAT, level=logging.INFO)
@@ -8,6 +10,9 @@ logging.basicConfig(format=settings.DEFAULT_LOG_FORMAT, level=logging.INFO)
 def create_application():
     application = Flask(__name__)
     application.config.from_object(settings)
+
+    metrics = PrometheusMetrics(application)
+    metrics.info("app_info", "Datatron Docker Service", version="1.0.0")
 
     from app.routes.api import api  # noqa
     api.init_app(application)
